b69c089ec36e8c8595a7a400a13246c41fb0cb1d
Robert Court <robbie.court@gmail.com>
2017-02-24 02:04:08 +0000

    swapping loading order

--- runservers.sh
+++ runservers.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 export JAVA_HOME=/etc/alternatives/jre_openjdk
+source "/root/.sdkman/bin/sdkman-init.sh"
 cd /opt/redis-stable/utils
 echo -n | ./install_server.sh
 cd /opt/aberowl-meta/aberowl-web
 ./start-server.sh &
-cd /opt/aberowl-meta/aberowl-server
-source "/root/.sdkman/bin/sdkman-init.sh"
-groovy AberOWLServer.groovy 31337 &
 cd /opt/aberowl-meta/aberowl-sync
 groovy RemoteOntologyDiscover.groovy
 groovy RemoteOntologyUpdate.groovy
+cd /opt/aberowl-meta/aberowl-server
+groovy AberOWLServer.groovy 31337
