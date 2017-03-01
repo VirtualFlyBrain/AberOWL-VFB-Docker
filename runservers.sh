58534a46e90c469924115cb8c395f354996d512d
Rob Court <robbie.court@gmail.com>
2017-03-01 09:52:48 +0000

    corrected update routine

--- runservers.sh
+++ runservers.sh
@@ -12,8 +12,10 @@ cd /opt/redis-stable/utils
 echo -n | ./install_server.sh &
 cd /opt/aberowl-meta/aberowl-web
 ./start-server.sh &
-cd /opt/aberowl-meta/aberowl-sync/
-groovy ReloadAll.groovy
-groovy UpdateAll.groovy
+cd /opt/aberowl-meta/jenkins/workspace
+groovy CheckUpdate.groovy VFB
+groovy Classify.groovy VFB
+groovy Index.groovy VFB
+groovy Restart.groovy VFB
 cd /opt/aberowl-meta/aberowl-server
 groovy AberOWLServer.groovy 31337
