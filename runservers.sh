40b7ba4fc8482be82daf56cf3ffcccd21517f97a
Rob Court <robbie.court@gmail.com>
2017-03-13 14:14:05 +0000

    removing install

--- runservers.sh
+++ runservers.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-export JAVA_HOME=/etc/alternatives/jre_openjdk
-export SDKMAN_DIR=/opt/.sdkman
-env HOME=/opt
-whoami
-echo ~
-curl -s get.sdkman.io | bash -
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install groovy
 cd /opt/redis-stable/utils
 echo -n | ./install_server.sh &
 cd /opt/aberowl-meta/jenkins/workspace
