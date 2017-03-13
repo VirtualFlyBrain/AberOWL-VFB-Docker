4a1d499e44b030acd7568f5e665583840623e85e
Rob Court <robbie.court@gmail.com>
2017-03-13 18:34:23 +0000

    converting to array

--- runservers.sh
+++ runservers.sh
@@ -4,7 +4,7 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 cd /opt/redis-stable/utils
 echo -n | ./install_server.sh &
 sleep 30s
-redis-cli set "ontologies:VFB" "{\"id\":\"VFB\",\"name\":\"VFB\",\"description\":\"VFB individuals\",\"homepage\":\"http://virtualflybrain.org\",\"source\":\"https://raw.githubusercontent.com/VirtualFlyBrain/VFB_owl/Current/src/owl/vfb.owl.gz\",\"status\":\"tested\",\"purl\":null,\"ncbi_id\":null,\"submissions\":{},\"owners\":[\"Robbie\"],\"species\":\"Drosophila\",\"topics\":null,\"contact\":\"support@virtualflybrain.org\",\"lastSubDate\":1462543726}"
+redis-cli set "ontologies:VFB" "{\"id\":\"VFB\",\"name\":\"VFB\",\"description\":\"VFB individuals\",\"homepage\":\"http://virtualflybrain.org\",\"source\":\"https://raw.githubusercontent.com/VirtualFlyBrain/VFB_owl/Current/src/owl/vfb.owl.gz\",\"status\":\"tested\",\"purl\":null,\"ncbi_id\":null,\"submissions\":{},\"owners\":[\"Robbie\"],\"species\":[\"Drosophila\"],\"topics\":null,\"contact\":[\"support@virtualflybrain.org\"],\"lastSubDate\":1462543726}"
 cd /opt/aberowl-meta/jenkins/workspace
 groovy CheckUpdate.groovy VFB
 groovy Classify.groovy VFB
