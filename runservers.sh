fdaff61fd494653bd8c4d9d293210272d9b254a2
Rob Court <robbie.court@gmail.com>
2017-03-14 06:15:12 +0000

    forcing file move

--- runservers.sh
+++ runservers.sh
@@ -7,8 +7,7 @@ sleep 30s
 redis-cli SET "ontologies:VFB" "{\"id\":\"VFB\",\"name\":\"VFB\",\"description\":\"VFB individuals\",\"homepage\":\"http://virtualflybrain.org\",\"source\":\"https://raw.githubusercontent.com/VirtualFlyBrain/VFB_owl/Current/src/owl/vfb.owl.gz\",\"status\":\"tested\",\"purl\":null,\"ncbi_id\":null,\"submissions\":{},\"owners\":[\"Robbie\"],\"species\":[\"Drosophila\"],\"topics\":null,\"contact\":[\"support@virtualflybrain.org\"],\"lastSubDate\":1462543726}"
 cd /opt/aberowl-meta/jenkins/workspace
 groovy CheckUpdate.groovy VFB
-groovy Classify.groovy VFB
-groovy Index.groovy VFB
+mv /opt/aberowl-meta/ontologies/VFB/new/VFB-raw.owl /opt/aberowl-meta/aberowl-server/onts/VFB_1.ont
 redis-cli SET "ontologies:VFB" "{\"id\":\"VFB\",\"name\":\"VFB\",\"description\":\"VFB individuals\",\"homepage\":\"http://virtualflybrain.org\",\"source\":\"https://raw.githubusercontent.com/VirtualFlyBrain/VFB_owl/Current/src/owl/vfb.owl.gz\",\"status\":\"tested\",\"purl\":null,\"ncbi_id\":null,\"submissions\":{\"1489440292\":\"VFB_1.ont\"},\"owners\":[\"Robbie\"],\"species\":[\"Drosophila\"],\"topics\":null,\"contact\":[\"support@virtualflybrain.org\"],\"lastSubDate\":1489440292}"
 redis-cli GET "ontologies:VFB"
 cd /opt/aberowl-meta/aberowl-server
